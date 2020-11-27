8df71e926c632d4185081fce88622302caaac64f
Paul Jefferies <jefferie@umich.edu>
2020-11-27 00:02:22 -0500

    Configuration File Established for Updates While Running

---
+++ Raspberry_Pi/balance_bot_config.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+"""
+Balance Bot Configuration Data.
+
+"""
+
+WHEEL_L_FWD = "GPIO4"     # Physical Pin  7 (1, 4)
+WHEEL_L_RWD = "GPIO17"    # Physical Pin 11 (1, 6)
+
+WHEEL_R_FWD = "GPIO27"    # Physical Pin 13 (1, 7)
+WHEEL_R_RWD = "GPIO22"    # Physical Pin 15 (1, 8)
+
+ARM_L_FR_LIFT = "GPIO5"   # Physical Pin 29 (1, 15)
+ARM_L_FR_DROP = "GPIO6"   # Physical Pin 31 (1, 16)
+
+ARM_R_FR_LIFT = "GPIO13"  # Physical Pin 33 (1, 17)
+ARM_R_FR_DROP = "GPIO19"  # Physical Pin 35 (1, 18)
+
+WHEEL_L_ENC = "GPIO26"    # Physical Pin 37 (1, 19)
+WHEEL_R_ENC = "GPIO21"    # Physical Pin 40 (2, 20)
+
+ARM_L_ENC = "GPIO20"      # Physical Pin 38 (2, 19)
+ARM_R_ENC = "GPIO16"      # Physical Pin 36 (2, 18)
+
+CONTROL_UPDATE_INTERVAL = 10  # milliseconds
+
+PARAMS_UPDATE_INTERVAL = 60  # seconds
+
+MAX_STEP_TIME = 60  # seconds
+
+K_PROPORTIONAL = 100  # Proportional Constant
+K_INTEGRAL = 2        # Helf catch-up
+K_DERIVATIVE = 0      # Damping Adjustment. Should be negative
+
+MOTOR_MAX = 1
+MOTOR_MIN = -1
