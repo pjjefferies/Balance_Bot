3dff08445d6b82ca762f5ab4e0e77f0df7617af3
Paul Jefferies <jefferie@umich.edu>
2020-11-27 00:10:52 -0500

    Rename bluedot function and add module simple test

--- Raspberry_Pi/bluedot_direction_control.py
+++ Raspberry_Pi/bluedot_direction_control.py
@@ -8,17 +8,20 @@ bd = BlueDot()
 
 bd.wait_for_connection()
 
-def drive():
+def bd_drive():
     if bd.is_pressed:
         x, y = bd.position.x, bd.position.y
         return x, y
     else:
         return 0, 0
 
-prev_position = [101, 101]
-while True:
-    position = list(drive())
-    if position != prev_position:
-        print(rf'x:{position[0]}, y:{position[1]}')
-    prev_position = position
-    sleep(0.2)
+
+if __name__ == '__main__':
+    prev_position = [101, 101]  # start with impossible position for comp.
+    
+    while True:
+        position = list(bd_drive())
+        if position != prev_position:
+            print(rf'x:{position[0]}, y:{position[1]}')
+        prev_position = position
+        sleep(0.2)
