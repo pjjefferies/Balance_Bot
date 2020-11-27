d92379ee8715b83202e423b8fbde460271e99ca4
Paul Jefferies <jefferie@umich.edu>
2020-11-27 00:13:15 -0500

    simple_robot created for bluedot steering of two wheeled robot

--- Raspberry_Pi/simple_robot.py
+++ Raspberry_Pi/simple_robot.py
@@ -1,40 +1,71 @@
 #! /usr/bin/python3
 
 import time
-import rpi_motor
-import bluedot_return_command
-
-
-MOTOR_L_IN_1 = 7
-MOTOR_L_IN_2 = 11
-MOTOR_L_ENABLE = 25
-MOTOR_R_IN_1 = 13
-MOTOR_R_IN_2 = 15
-MOTOR_R_ENABLE = ?
+from gpiozero import Motor
+import bluedot_direction_control
 
 
 class Simple_Robot:
-	"""
-	Basic Robot Manual control for 2-Motor Simple Robot
-	"""
-	def __init__(self):
-		GPIO.setwarnings(False)
-		#set GPIO numbering mode and define output pins
-		GPIO.setmode(GPIO.BOARD)
+    """
+    Basic Robot Manual control for 2-Motor Simple Robot
+    """
+    def __init__(self, left_forward_line, left_backward_line,
+                 right_forward_line, right_backward_line):
+        """
 
-		self.motor_l = Motor(MOTOR_L_IN_1,
-			  				 MOTOR_L_IN_2,
-					 		 MOTOR_L_ENABLE)
-		self.motor_r = Motor(MOTOR_R_IN_1,
-							 MOTOR_R_IN_2,
-							 MOTOR_R_IN_2)
-		self.bd_ctl = bluedot_direction_control()
 
-	def move_in_bd_dir(time_step=1):  # time_step in seconds
-		start_time = time.time()
-		direction = self.bd_ctl()  # [x, y] eached scaled [-1, 1]
-		if direction == [0, 0]:
-			self.motor_l.stop()
-			self.motor_r.stop()
-		else:
-			
+        Args:
+            left_forward_line (TYPE): DESCRIPTION.
+            left_backward_line (TYPE): DESCRIPTION.
+            right_forward_line (TYPE): DESCRIPTION.
+            right_backward_line (TYPE): DESCRIPTION.
+
+        Returns:
+            None.
+
+        """
+        self.motor_l = Motor(forward=left_forward_line,
+                             backward=left_backward_line,
+                             pwm=True)
+        self.motor_r = Motor(forward=right_forward_line,
+                             backward=right_backward_line,
+                             pwm=True)
+        self.bd_ctl = bluedot_direction_control.bd_drive
+
+    def drive_two_wheel_robot_by_bd(self, interval=500, duration=60):  # ms, s
+        start_time = time.time()
+
+        lasttime = 0
+
+        while time.time() - start_time < duration:
+            if (time.time()*1000 - lasttime) >= interval:
+                lasttime = time.time() * 1000
+                direction = self.bd_drive()  # [x, y] eached scaled [-1, 1]
+                if direction == [0, 0]:
+                    self.motor_l.value = 0
+                    self.motor_r.value = 0
+                else:
+                    fwd_vel = direction[0]
+                    right_turn = direction[1]
+                    self.motor_l.value = fwd_vel + right_turn
+                    self.motor_r.value = fwd_vel - right_turn
+
+
+if __name__ == "__main__":
+    MOTOR_L_FWD = 7
+    MOTOR_L_RWD = 11
+    # MOTOR_L_ENABLE = 25
+    MOTOR_R_FWD = 13
+    MOTOR_R_RWD = 15
+    # MOTOR_R_ENABLE = ?
+
+    ROBOT_RUN_DURATION = 60  # Seconds
+    CONTROL_STEPS = 500  # milliseconds
+
+    two_wheel_robot = Simple_Robot(MOTOR_L_FWD,
+                                   MOTOR_L_RWD,
+                                   MOTOR_R_FWD,
+                                   MOTOR_R_RWD)
+
+    two_wheel_robot.drive_two_wheel_robot_by_bd(interval=CONTROL_STEPS,
+                                                duration=ROBOT_RUN_DURATION)
