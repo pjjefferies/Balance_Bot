75ff789d7eec6959976d28354b04aaac3c1db547
Paul Jefferies <jefferie@umich.edu>
2020-11-27 00:21:36 -0500

    simple_robot updated to use balance_bot_config for set-up and import encoder sensor but not use yet

--- Raspberry_Pi/simple_robot.py
+++ Raspberry_Pi/simple_robot.py
@@ -3,33 +3,39 @@
 import time
 from gpiozero import Motor
 import bluedot_direction_control
+from encoder_sensor import RotationEncoder
+import balance_bot_config as bbc
 
 
 class Simple_Robot:
     """
     Basic Robot Manual control for 2-Motor Simple Robot
     """
-    def __init__(self, left_forward_line, left_backward_line,
-                 right_forward_line, right_backward_line):
+    def __init__(self):
         """
-
+        Initialize Simple Robot using values in balance_bot_config
 
         Args:
-            left_forward_line (TYPE): DESCRIPTION.
-            left_backward_line (TYPE): DESCRIPTION.
-            right_forward_line (TYPE): DESCRIPTION.
-            right_backward_line (TYPE): DESCRIPTION.
+            None
 
         Returns:
             None.
 
         """
-        self.motor_l = Motor(forward=left_forward_line,
-                             backward=left_backward_line,
-                             pwm=True)
-        self.motor_r = Motor(forward=right_forward_line,
-                             backward=right_backward_line,
-                             pwm=True)
+        # Set-up Motor Control Pins
+        self._motor_wheel_left = Motor(forward=bbc.WHEEL_L_FWD,
+                                       backward=bbc.WHEEL_L_RWD,
+                                       pwm=True)
+        self._motor_wheel_right = Motor(forward=bbc.WHEEL_R_FWD,
+                                        backward=bbc.WHEEL_R_RWD,
+                                        pwm=True)
+
+        # Set-up Motor Encoders
+        self._enc_wheel_left = RotationEncoder(signal_pin=bbc.WHEEL_L_ENC)
+        self._enc_wheel_right = RotationEncoder(signal_pin=bbc.WHEEL_R_ENC)
+        self._enc_arm_left = RotationEncoder(signal_pin=bbc.ARM_L_ENC)
+        self._enc_arm_right = RotationEncoder(signal_pin=bbc.ARM_R_ENC)
+
         self.bd_ctl = bluedot_direction_control.bd_drive
 
     def drive_two_wheel_robot_by_bd(self, interval=500, duration=60):  # ms, s
@@ -42,30 +48,21 @@ class Simple_Robot:
                 lasttime = time.time() * 1000
                 direction = self.bd_drive()  # [x, y] eached scaled [-1, 1]
                 if direction == [0, 0]:
-                    self.motor_l.value = 0
-                    self.motor_r.value = 0
+                    self._motor_wheel_left.value = 0
+                    self._motor_wheel_right.value = 0
                 else:
                     fwd_vel = direction[0]
                     right_turn = direction[1]
-                    self.motor_l.value = fwd_vel + right_turn
-                    self.motor_r.value = fwd_vel - right_turn
+                    self._motor_wheel_left.value = fwd_vel + right_turn
+                    self._motor_wheel_right.value = fwd_vel - right_turn
 
 
 if __name__ == "__main__":
-    MOTOR_L_FWD = 7
-    MOTOR_L_RWD = 11
-    # MOTOR_L_ENABLE = 25
-    MOTOR_R_FWD = 13
-    MOTOR_R_RWD = 15
-    # MOTOR_R_ENABLE = ?
 
     ROBOT_RUN_DURATION = 60  # Seconds
     CONTROL_STEPS = 500  # milliseconds
 
-    two_wheel_robot = Simple_Robot(MOTOR_L_FWD,
-                                   MOTOR_L_RWD,
-                                   MOTOR_R_FWD,
-                                   MOTOR_R_RWD)
+    two_wheel_robot = Simple_Robot()
 
     two_wheel_robot.drive_two_wheel_robot_by_bd(interval=CONTROL_STEPS,
                                                 duration=ROBOT_RUN_DURATION)
