acd0fab7e742fcb642c053b8345a089ac1b53427
Paul Jefferies <jefferie@umich.edu>
2020-12-21 00:29:56 -0500

    Update sensor_test.py to get all sensor info, just need to save it now

--- Raspberry_Pi/sensor_test.py
+++ Raspberry_Pi/sensor_test.py
@@ -15,19 +15,46 @@ import logging
 import board
 import busio
 import adafruit_bno055
+import balance_bot_config as bbc
 
 
 # Initialize Logging
-format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.INFO,
-                    datefmt="%Y-%m-%d : %H:%M:%S")
+LOG_FORMAT = '%(asctime)s — %(name)s — %(levelname)s — %(message)s'
+logging.basicConfig(format=LOG_FORMAT,
+                    level=bbc.LOG_LEVEL,
+                    datefmt="%Y-%m-%dT%H:%M:%S")
+logger = logging.getLogger(__name__)
 
 # Initialize i2C Connection to sensor- need check/try?
 i2c = busio.I2C(board.SCL, board.SCA)
-self._sensor = adafruit_bno055.BNO055_I2C(i2c)
-try:
-    accel_x, accel_y, accel_z = (
-        self._sensor.linear_acceleration)
-except RuntimeError:
-    return False
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
+UPDATE_TIME = 1  # Seconds
+
+try:
+    accel_x, accel_y, accel_z = sensor.linear_acceleration
+except RuntimeError:
+    logger.ERROR('No Sensor Acceleration detected')
+    raise
+
+    try:
+        LASTTIME_CONTROL = 0
+        while True:
+            if ((time.time() - LASTTIME_CONTROL) >= UPDATE_TIME):
+                # exec every UPDATE_TIME seconds
+                lasttime_control = time.time()
+                accel_x, accel_y, accel_z = sensor.linear_acceleration
+                roll, pitch, yaw = sensor.euler
+                temperature = sensor.temperature
+                magnetic = sensor.magnetic
+                gyro = sensor.gyro
+                gravity = sensor.gravity
+
+                """
+                logger.debug(f'Pos.: {POSITION:.2f}, ' +
+                             'Spe.: {SPEED:.2f}, ' +
+                             'Acc.: {ACCEL:.2f}, ' +
+                             'Jrk.: {JERK: .2f}')
+                """
+    except KeyboardInterrupt:
+        pass
