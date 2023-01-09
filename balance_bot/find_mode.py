a7cf86e800ac1a6aedd67d06dd3db372c431193b
Paul Jefferies <jefferie@umich.edu>
2023-01-08 21:57:21 -0500

    Last develpment commit of find_mode.py

--- balance_bot/find_mode.py
+++ balance_bot/find_mode.py
@@ -30,7 +30,7 @@ def main():
             0.0409477,
             0.1015785,
             0.0410812,
-            0.0409825,
+            0.0409825,  # 10
             0.0409279,
             0.040946,
             0.0712287,
@@ -55,63 +55,17 @@ def main():
     def gamma_density(data: float, *params: Tuple[float, float, float]):
         return -stats.gamma.pdf(data, *params)[0]
 
-    # mode1 = gamma_mode(data1)
-    params: Tuple[float, float, float] = stats.gamma.fit(data=data)
     a: float  # shape
     loc: float  # location
     scale: float  # scale (theta)
-    a, loc, scale = params
+    a, loc, scale = stats.gamma.fit(data=data)
     print(f"a: {a}, loc: {loc}, scale: {scale}")
 
-    min_result = minimize(
+    mode = minimize(
         fun=gamma_density, x0=0.05, args=(a, loc, scale), method="COBYLA"
-    )
-
-    # t = timeit.Timer(lambda: foo(A, B))
-
-    t = timeit.Timer(
-        lambda: minimize(
-            fun=gamma_density, x0=0.05, args=(a, loc, scale), method="Powell"
-        ),
-    )
-    duration = t.timeit(number=1000)
-    print(f"Powell duration: {duration:.2f} seconds")
-
-    t = timeit.Timer(
-        lambda: minimize(
-            fun=gamma_density, x0=0.05, args=(a, loc, scale), method="TNC"
-        ),
-    )
-    duration = t.timeit(number=1000)
-    print(f"TNC duration: {duration} seconds")
-
-    t = timeit.Timer(
-        lambda: minimize(
-            fun=gamma_density, x0=0.05, args=(a, loc, scale), method="COBYLA"
-        ),
-    )
-    duration = t.timeit(number=1000)
-    print(f"COBYLA duration: {duration} seconds")
-
-    # Powell - correct resuts, success=True, time=0.28 s
-    # TNC - correct result, success=True, time=0.16 s
-    # COBYLA - correct result, success=True, time=0.025 s
-
-    # Nelder-Mead - correct result, success=False
-    # CG - incorrect result, success=False
-    # BFGS - incorrect result, success=False
-    # Newton-CG - couldn't run
-    # L-BFGS-B - close result, success=False
-    # SLSQP - incorrect result, success=True  #SCARY
-    # trust-constr - correct result, success=True but received warning
-    # dogleg - couldn't run
-    # trust-ncg - couln't run
-    # trust-exact - couldn't run
-    # trust-krylov - couldn't run
-
-    print(f"min_result: {min_result}")
-
-    mode = min_result.x
+    ).x[
+        0
+    ]  # COBYLA - correct result, success=True, time=0.025 s - see initial commit of file for other method results
 
     print(f"The mode of data1 is: {mode}.")
 
@@ -123,16 +77,15 @@ def main():
         histtype="stepfilled",
         alpha=0.2,
     )
-    # x = np.linspace(stats.gamma.ppf(0.01, a), stats.gamma.ppf(0.99, a), 100)
     x = np.linspace(0.001, 0.125, 1000)
 
     ax.plot(
         x, stats.gamma.pdf(x, a, loc, scale), "r-", lw=2, alpha=0.6, label="gamma pdf"
     )
-
+    ax.text(0.06, 40, s="".join(["Mode: ", str(round(mode, 4))]))
     ax.set_xlim([x[0], x[-1]])
     ax.legend(loc="best", frameon=False)
-    # plt.show()
+    plt.show()
 
 
 if __name__ == "__main__":
