b78adf637d7d8fdcb28f6c4a605324256440cc5c
Paul Jefferies <jefferie@umich.edu>
2023-02-10 23:51:41 -0500

    Comulative Average module/function created

---
+++ balance_bot/cumulative_average.py
@@ -0,0 +1,12 @@
+"""
+Calulate Cumulative Average of series based on previous running
+average, new value and number of history points
+"""
+
+
+def cumulative_average(
+    *, prev_cumulative_average: float, new_value: float, number_of_history_points: int
+):
+    return (
+        new_value + (number_of_history_points - 1) * prev_cumulative_average
+    ) / number_of_history_points
