072bf8f8daef9b71d877795431fb39da3c17e630
Paul Jefferies <jefferie@umich.edu>
2023-02-26 23:39:31 -0500

    Change logging to use date-time at subscribe time for filename

--- balance_bot/event.py
+++ balance_bot/event.py
@@ -3,6 +3,7 @@
 # Based on Observer Pattern Tutorial by ArjanCodes on YouTube
 # https://www.youtube.com/watch?v=oNalXg67XEE&t=0s
 
+from functools import partial
 from typing import Callable, Optional, Any
 
 
@@ -26,11 +27,17 @@ class EventHandler:
         self,
         *,
         event_type: str,
-        fn: Callable[[str, str | Any, Optional[str]], None],
+        fn: Callable[[str, str | Any, Optional[str], Optional[str]], None],
+        dt_string: Optional[str] = None,
     ) -> None:
+        new_fn: Callable[[str, str | Any, Optional[str]], None]
+        if dt_string is not None:
+            new_fn = partial(fn, dt_string=dt_string)
+        else:
+            new_fn = fn
         if event_type not in self._subscribers:
             self._subscribers[event_type] = []
-        self._subscribers[event_type].append(fn)
+        self._subscribers[event_type].append(new_fn)
 
     def post(
         self,
--- balance_bot/robot_listener.py
+++ balance_bot/robot_listener.py
@@ -14,15 +14,19 @@ from balance_bot.event import EventHandler
 
 # General Log File Event Handler
 def general_logfile_eh(
-    event_type: str, message: str, level: Optional[str] = None
+    event_type: str,
+    message: str,
+    level: Optional[str] = None,
+    dt_string: Optional[str] = None,
 ) -> None:
     general_eh_handler = "log_file"
-
-    now = dt.datetime.now()
+    now: str = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    if dt_string is None:
+        dt_string = now
     level_text = ": " + level if level is not None else ""
     log_folder = log_cfg.handler[general_eh_handler].folder
     log_filename_base = log_cfg.handler[general_eh_handler].filename
-    log_filename = f"{now:%Y-%m-%d_%H_%M_%S}_{log_filename_base}"
+    log_filename = f"{dt_string}_{log_filename_base}"
     log_filename_path = os.path.join(log_folder, log_filename)
 
     with open(log_filename_path, "a") as f:
@@ -39,22 +43,27 @@ def general_logfile_eh(
 
 # General Stdout Event Handler
 def general_stdout_eh(
-    event_type: str, message: str, level: Optional[str] = None
+    event_type: str,
+    message: str,
+    level: Optional[str] = None,
 ) -> None:
-    now = dt.datetime.now()
     level_text = ": " + level if level is not None else ""
-    # log_folder = log_cfg.handler[general_eh_handler].folder
-    # log_filename_base = log_cfg.handler[general_eh_handler].filename
     print(
         log_cfg.format.simple.format(
-            now=now, event_type=event_type, level_text=level_text, message=message
+            now=f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}",
+            event_type=event_type,
+            level_text=level_text,
+            message=message,
         ),
         file=sys.stdout,
     )
 
 
 def setup_robot_movement_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="robot moved", fn=general_logfile_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(
+        event_type="robot moved", fn=general_logfile_eh, dt_string=subscribe_time
+    )
     eh.subscribe(event_type="robot moved", fn=general_stdout_eh)
 
 
@@ -64,7 +73,10 @@ def robot_encoder_sensor_eh(message: str) -> None:
 
 
 def setup_robot_encoder_sensor_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="encoder sensor", fn=general_logfile_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(
+        event_type="encoder sensor", fn=general_logfile_eh, dt_string=subscribe_time
+    )
 
 
 # Robot 9DOF Sensor Event Handler
@@ -73,8 +85,13 @@ def robot_9DOF_sensor_eh(message: str) -> None:
 
 
 def setup_robot_9DOF_sensor_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="9DOF sensor", fn=general_logfile_eh)
-    eh.subscribe(event_type="9DOF sensor", fn=general_stdout_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(
+        event_type="9DOF sensor", fn=general_logfile_eh, dt_string=subscribe_time
+    )
+    eh.subscribe(
+        event_type="9DOF sensor", fn=general_stdout_eh, dt_string=subscribe_time
+    )
 
 
 # BlueDot Event Handler
@@ -83,7 +100,8 @@ def bluedot_eh(message: str) -> None:
 
 
 def setup_bluedot_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="bluedot", fn=general_logfile_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(event_type="bluedot", fn=general_logfile_eh, dt_string=subscribe_time)
 
 
 # Power Event Handler
@@ -92,8 +110,9 @@ def power_eh(message: str) -> None:
 
 
 def setup_power_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="power", fn=general_logfile_eh)
-    eh.subscribe(event_type="power", fn=general_stdout_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(event_type="power", fn=general_logfile_eh, dt_string=subscribe_time)
+    eh.subscribe(event_type="power", fn=general_stdout_eh, dt_string=subscribe_time)
 
 
 # General Logging Event Handlers
@@ -111,7 +130,8 @@ def general_logging_handler(message: str) -> None:
 
 
 def setup_general_logging_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="log", fn=general_logfile_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(event_type="log", fn=general_logfile_eh, dt_string=subscribe_time)
 
 
 # Position History Event Handler
@@ -128,4 +148,7 @@ def position_history_eh(
 
 
 def setup_position_history_logging_handler(eh: EventHandler) -> None:
-    eh.subscribe(event_type="position_history", fn=position_history_eh)
+    subscribe_time = f"{dt.datetime.now():%Y-%m-%d_%H_%M_%S}"
+    eh.subscribe(
+        event_type="position_history", fn=position_history_eh, dt_string=subscribe_time
+    )
