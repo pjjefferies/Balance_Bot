aec43cf11917cd93408545a42d637606235e092c
Paul Jefferies <jefferie@umich.edu>
2023-03-10 21:34:40 -0500

    From test_BNO055_sensor, remove superfluous lines

--- tests/function/test_BNO055_sensor.py
+++ tests/function/test_BNO055_sensor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
 from abc import abstractmethod
-import asyncio
 import time
-from typing import Awaitable, Callable, Optional, Protocol
+from typing import Callable, Protocol
 
 import board
 from box import Box
@@ -11,10 +10,7 @@ import busio
 
 
 from balance_bot import bb_bno055_sensor as bno055
-from balance_bot.config_main import load_config
 from balance_bot.event import EventHandler
-from balance_bot.motor_battery_relay import MotorBatteryRelay
-from balance_bot.rpi_motor import RPI_Motor
 from balance_bot import robot_listener
 
 
@@ -66,10 +62,7 @@ TIME_S: Callable[[], int] = lambda: int(time.time())
 
 
 def test_bno055_sensor():
-    """
-    Test BNO055 Sensor
-    """
-    cfg: Box = load_config()
+    """Test BNO055 Sensor"""
     eh = EventHandler()
 
     robot_listener.setup_robot_9DOF_sensor_handler(eh=eh)
@@ -93,8 +86,7 @@ def test_bno055_sensor():
 
     while True:
         try:
-            temp_euler: dict[str, float] = sensor.euler_angles
-            # eh.post(event_type="9DOF sensor", message=f"Euler Angles:\n{temp_euler}")
+            sensor.euler_angles
             time.sleep(1)
         except KeyboardInterrupt:
             break
